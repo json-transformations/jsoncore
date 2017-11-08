1fa9ebf936087ddfb282725a00e12a5ac6e21879
Brian Peterson <bpeterso2000@yahoo.com>
2017-11-07 23:24:34 -0500

    Fix tests

--- tests/test_apply.py
+++ tests/test_apply.py
@@ -1,4 +1,5 @@
-from jsoncore.apply import apply_funct, apply_keys, group_arrays, splitlist
+from jsoncore.mapfilter import (apply_funct, apply_keys, group_arrays,
+                                map_values, splitlist)
 from jsoncore.core import get_value, set_value
 
 
@@ -26,7 +27,7 @@ def test_apply_funct():
     def map_values(keys, funct, seq):
         def apply_funct2keys(item):
             for key in keys:
-                set_value(item, key, funct(get_value(item, key)))
+                set_value(key, funct(get_value(key, item)), item)
             return item
         result = list(map(apply_funct2keys, seq))
         return result
@@ -65,17 +66,16 @@ def test_apply_funct():
                         ]
                     }]
               }]}}
-    expect = [[[{'name': 'CLUSTRIL', 'diameter (km)': 3.4},
-                {'name': "D'ARREST", 'diameter (km)': 2.1}],
-               [{'name': 'SWIFT', 'diameter (km)': 1},
-                {'name': 'VOLTAIRE', 'diameter (km)': 1.9}]
-               ], [[
-                {'name': 'GAEA','diameter (km)': 80},
-                {'name': 'PAN', 'diameter (km)': 100}
-               ], [
-                {'name': 'ADAL', 'diameter (km)': 41.7},
-                {'name': 'AEGIR', 'diameter (km)': 53.9}
-             ]]]
+    expect = {'Solar System': {'planets': [{'name': 'Mars', 'moons': [{'name':
+              'Phobos', 'craters': [{'name': 'CLUSTRIL', 'diameter (km)': 3.4},
+              {'name': "D'ARREST", 'diameter (km)': 2.1}]}, {'name': 'Deimos',
+              'craters': [{'name': 'SWIFT', 'diameter (km)': 1}, {'name':
+              'VOLTAIRE', 'diameter (km)': 1.9}]}]}, {'name': 'Jupiter',
+              'moons': [{'name': 'Amalthea', 'craters': [{'name': 'GAEA',
+              'diameter (km)': 80}, {'name': 'PAN', 'diameter (km)': 100}]},
+              {'name': 'Callisto', 'craters': [{'name': 'ADAL',
+              'diameter (km)': 41.7}, {'name': 'AEGIR', 'diameter (km)': 53.9
+              }]}]}]}}
     groups = [('Solar System', 'planets'), ('moons',)]
     array = ('craters',)
     keys = [('name',)]
@@ -84,17 +84,6 @@ def test_apply_funct():
 
 
 def test_apply_keys():
-
-    def map_values(keys, funct, seq):
-        def apply_funct2keys(item):
-            for key in keys:
-                set_value(item, key, funct(get_value(item, key)))
-            return item
-        result = list(map(apply_funct2keys, seq))
-        return result
-
-    def capitalize(keys, data):
-        return map_values(keys, str.upper, data)
 
     data = {"Solar System": {"planets": [{"name": "Mars", "moons": [{"name":
         "Phobos", "craters": [{"name": "Clustril", "diameter (km)": 3.4},
@@ -118,5 +107,18 @@ def test_apply_keys():
         'name'], ['Solar System', 'planets', '*', 'moons', '*', 'name'],
         ['Solar System', 'planets', '*', 'name']]
 
+
+    def map_values(keys, funct, seq):
+        def apply_funct2keys(item):
+            for key in keys:
+                set_value(key, funct(get_value(key, item)), item)
+            return item
+        result = list(map(apply_funct2keys, seq))
+        return result
+
+    def capitalize(keys, data):
+        return map_values(keys, str.upper, data)
+
     result = apply_keys(keys, capitalize, data)
+    print(result)
     assert result == expect
