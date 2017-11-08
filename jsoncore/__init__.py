95bc9ccf07823478741c01494061fa6d6b49a6b5
Brian Peterson <bpeterso2000@yahoo.com>
2017-11-07 23:24:11 -0500

    Remove old modules & functs

--- jsoncore/__init__.py
+++ jsoncore/__init__.py
@@ -1,7 +1,5 @@
 """jsoncore - Package description."""
-from ._compat import suppress
-from .core import get_value, get_item, set_value, del_key
-from .core import jsonkeys, jsonvalues, jsonitems
-from .parse import REGEX
+
+from .jsonfuncts import jsonkeys
 
 __version__ = '0.5'
