a46084a5bad42bdc629098f7da392a83091bb292
Brian Peterson <bpeterso2000@yahoo.com>
2017-11-07 23:23:13 -0500

    Add map, filter & reduce

--- jsoncore/_compat.py
+++ jsoncore/_compat.py
@@ -3,14 +3,17 @@ import sys
 PY3 = sys.version_info.major == 3
 
 if PY3:
-    import functools
     from contextlib import suppress
     from io import StringIO
+    from itertools import filterfalse
     from functools import reduce
+    string_type, text_type = (str, str)
 else:
     from contextlib2 import suppress
+    from itertools import ifilter as filter
+    from itertools import ifilterfalse as filterfalse
+    from itertools import imap as map
     from StringIO import StringIO
+    string_type, text_type = (str, unicode)
 
-reduce_ = reduce
-
-string_type, text_type = (str, str) if PY3 else (str, unicode)
+map, filter, reduce = map, filter, reduce
