a405baa64f8f535100099ba0f8627cfa79e2f021
Brian Peterson <bpeterso2000@yahoo.com>
2017-11-14 23:07:59 -0500

    Add new click type JsonFile

--- jsoncore/cli.py
+++ jsoncore/cli.py
@@ -1,20 +1,32 @@
+import json
+import sys
+
 import click
-from click import argument, command, option, pass_context version_option
+import click._termui_impl
 
 
-@command()
-@argument('jsonfile', type=click.Path(readable=True), required=False)
-@option('-r', '--root', help='Select a JSON branch to process')
-@option('-l', '--list', 'list_', is_flag=True, help="List keys")
-@option('-a', '--all', is_flag=True, help='List all keys; include arrays')
-@option('-t', '--types', is_flag=True, help='List node types')
-@option('-c', '--compact', is_flag=True, help='Compact the JSON output')
-@option('-R', '--result', help='Select JSON branch to output after processing')
-@version_option(prog_name='jsoncore')
-@pass_context
-def main(ctx, **kwds):
-    ctx.color = False if kwds['nocolor'] else True
+class JSONFile(click.File):
+    name = 'JSON.load'
 
+    def __init__(self, **kwds):
+        super(JSONFile, self).__init__(**kwds)
 
-if __name__ == '__main__':
-    main()
+    def convert(self, value, param, ctx):
+        f = super(JSONFile, self).convert(value, param, ctx)
+        if click._termui_impl.isatty(sys.stdin):
+            click.echo(ctx.get_usage())
+            click.echo("Try `jsonclick --help' for more information.")
+            return ''
+        try:
+            return json.load(f)
+        except (json.JSONDecodeError, TypeError) as e:
+            self.fail('%s does not contain valid JSON: %s' % (
+                click._compat.filename_to_ui(value),
+                click._compat.get_streerror(e),
+            ), param, ctx)
+        except UnicodeError as e:
+            self.fail('%s: %s' % (
+                click._compat.filename_to_ui(value),
+                click._compat.get_streerror(e)
+            ), param, ctx)
+        return ''
