b51cad3efd0ddecd49c84515ffdbbe836f76b836
Brian Peterson <bpeterso2000@yahoo.com>
2017-11-16 16:35:59 -0500

    Refactor test

--- tests/test_map.py
+++ tests/test_map.py
@@ -1,7 +1,7 @@
 import json
 import os
 
-from jsoncore._compat import PY3
+from jsoncore._compat import text_type
 from jsoncore.mapreducefilter import map_values
 
 MODULE_PATH = os.path.dirname(os.path.realpath(__file__))
@@ -9,8 +9,7 @@ MODULE_PATH = os.path.dirname(os.path.realpath(__file__))
 
 def test_map_values():
     data = json.load(open(os.path.join(MODULE_PATH, 'data/nfl_arrests.json')))
-    # import pdb; pdb.set_trace()
-    result = map_values([('Team_name',)], str.upper if PY3 else unicode.upper, data)
+    result = map_values([('Team_name',)], getattr(text_type, 'upper'), data)
     assert result[0]['Team_name'] == 'VIKINGS'
     assert result[0]['Team_city'] == 'Minneapolis'
     assert result[1]['Team_name'] == 'BRONCOS'
@@ -23,7 +22,7 @@ def test_map_nested_values():
         ('entry', '*', 'resource', 'name', '*', 'family'),
         ('entry', '*', 'resource', 'name', '*', 'given', 0)
     ]
-    result = map_values(fields, str.upper if PY3 else unicode.upper, data)
+    result = map_values(fields, getattr(text_type, 'upper'), data)
     resource = result['entry'][10]['resource']
     assert resource['contact'][0]['gender'] == 'FEMALE'
     assert resource['name'][0]['family'] == 'CHALMERSCNVMSL'
