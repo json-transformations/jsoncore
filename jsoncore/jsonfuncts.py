db3821638b2dc36333a715a4424e6977d1d60b31
Brian Peterson <bpeterso2000@yahoo.com>
2017-11-28 21:09:13 -0500

    Finish building out json functions

--- jsoncore/jsonfuncts.py
+++ jsoncore/jsonfuncts.py
@@ -1,24 +1,18 @@
 from functools import wraps
 
 from toolz import curry
+
 from jsoncrawl import node_visitor
 
-from .core import WILDCARD, del_key
-
+from .core import (
+    WILDCARD, SUPPRESS, del_key, get_item, get_keys, get_nodes,
+    get_value, set_value
+)
 from .parse import parse_keys
 from .sequence import is_seq_and_not_str
 
 
-def jsonkeys(d, arrays=True):
-    """Return a set of unique keys found in a JSON document."""
-    def getkeys(node):
-        return node.keys
-
-    keys = node_visitor(d, getkeys, arrays=arrays, element_ch=WILDCARD)
-    return set(map(tuple, keys))
-
-
-def parse_keystr(func):
+def key_parser(func):
     @wraps(func)
     def wrapper(keys, *args, **kwds):
         if not is_seq_and_not_str(keys):
@@ -28,12 +22,50 @@ def parse_keystr(func):
 
 
 @curry
-@parse_keystr
-def jsondel(keys, *args, **kwds):
-    del_key(keys, *args, **kwds)
+@key_parser
+def jsonget(keylist, d, default=None, ignore=SUPPRESS):
+    return get_value(keylist, d, default=default, ignore=ignore)
 
 
-def jsonvalues(d, arrays=True, ignore=('object', 'array')):
-    """Return the value of each node in a JSON document."""
-    nodes = node_visitor(d, lambda x: x, arrays=arrays)
-    return (v for k, v, t in nodes if t not in ignore)
+@curry
+@key_parser
+def jsongetitem(keylist, d, default=None, fullpath=False, ignore=SUPPRESS):
+    return get_item(keylist, d, default=None, fullpath=False, ignore=SUPPRESS)
+
+
+@curry
+@key_parser
+def jsonset(keylist, value, d, ignore=SUPPRESS):
+    return set_value(keylist, value, d, ignore=ignore)
+
+
+@curry
+@key_parser
+def jsondel(keylist, d, ignore=SUPPRESS):
+    return del_key(keylist, d, ignore=ignore)
+
+
+def jsonnodes(d, wildcard=WILDCARD):
+    """Given JSON data; yield each node."""
+    for node in node_visitor(d, arrays=True, element_ch=wildcard):
+        if node.keys is not None:
+            yield node
+
+
+def jsonitems(d, wildcard=WILDCARD):
+    """Given JSON data; yield each keystring/value pair."""
+    for node in get_nodes(d, wildcard=wildcard):
+        yield node.keys, node.value
+
+
+def jsonkeys(d, wildcard=WILDCARD):
+    """Given JSON data; yield each keystring in sort order."""
+    for key in get_keys(d, wildcard=WILDCARD):
+        yield key
+
+
+def jsonvalues(d, arrays=True, wildcard=WILDCARD):
+    """Given JSON data; yield each value."""
+    for node in jsonnodes(d, wildcard=wildcard):
+        if node.dtype not in ('array', 'object'):
+            yield node.val
